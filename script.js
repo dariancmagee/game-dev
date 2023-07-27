1ea3c9248a1d8c3aa4d0e4749e05529a31da4b68
dariancmagee <dariancmagee@gmail.com>
2023-07-27 15:58:11 -0400

    player input

--- script.js
+++ script.js
@@ -7,38 +7,62 @@ window.addEventListener('load', function(){
 
     class InputHandler {
         
-
     }
     class Projectile {
-
 
     }
     class Particle {
 
-
     }
     class Player {
-        
-
+        constructor(game){
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+        }
+        update(){
+            this.y += this.speedY;
+        }
+        draw(context){
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
     class Enemy {
         
-
     }
     class Layer {
-
 
     }
     class Backgroung {
 
-
     }
     class UI {
 
-
     }
     class Game {
-
-
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+        }
+        update(){
+            this.player.update();
+        }
+        draw(context){
+            this.player.draw(context)
+        }
     }
+
+    const game = new Game(canvas.width, canvas.height);
+    // animation loop
+    function animate(){
+        ctx.clearRect(0,0, canvas.width, canvas.height)
+        game.update();
+        game.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate();
 });
